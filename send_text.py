eec696f699254f4f914194dfcf3d937a65715fdb
Brian Schuster <brian.daniel.schuster@gmail.com>
2014-06-19 18:23:19 -0400

    Added CMD Args to Program

--- send_text.py
+++ send_text.py
@@ -1,11 +1,14 @@
 from twilio.rest import TwilioRestClient
+from sys import argv 
+
+script, to_number , sms_message = argv
  
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "AC771e2dd643d0c43e3fcf35374866404d"
 auth_token  = "2a5d74b7b1aa24105d2533276b54221d"
 client = TwilioRestClient(account_sid, auth_token)
  
-message = client.messages.create(body="It's Brian. I'm sending this through a python program. No Joke.",
-    to="+13177717786",    # Replace with your phone number
+message = client.messages.create(body = sms_message,
+    to= to_number ,    # Replace with your phone number
     from_="+13176718700") # Replace with your Twilio number
 print message.sid
